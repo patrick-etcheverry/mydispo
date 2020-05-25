9d4b4372e22e3354d0d4b1e380d01640246c53e8
ThomasToto <thombouchet@aol.com>
2020-05-25 11:20:13 +0200

    Création de l'entité LogEnseignant

---
+++ MyDispo/src/Entity/LogEnseignant.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LogEnseignantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LogEnseignantRepository::class)
+ */
+class LogEnseignant
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $typeAction;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $zone;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTypeAction(): ?string
+    {
+        return $this->typeAction;
+    }
+
+    public function setTypeAction(string $typeAction): self
+    {
+        $this->typeAction = $typeAction;
+
+        return $this;
+    }
+
+    public function getZone(): ?string
+    {
+        return $this->zone;
+    }
+
+    public function setZone(?string $zone): self
+    {
+        $this->zone = $zone;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
---
+++ MyDispo/src/Repository/LogEnseignantRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\LogEnseignant;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method LogEnseignant|null find($id, $lockMode = null, $lockVersion = null)
+ * @method LogEnseignant|null findOneBy(array $criteria, array $orderBy = null)
+ * @method LogEnseignant[]    findAll()
+ * @method LogEnseignant[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LogEnseignantRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, LogEnseignant::class);
+    }
+
+    // /**
+    //  * @return LogEnseignant[] Returns an array of LogEnseignant objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('l.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?LogEnseignant
+    {
+        return $this->createQueryBuilder('l')
+            ->andWhere('l.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
