a8a4d81a6fa013aac136cd4daf40a83bf4199dc2
ThomasToto <thombouchet@aol.com>
2020-05-27 22:44:56 +0200

    Modification foormulaire modèle mail

--- MyDispo/src/Form/ModeleMailType.php
+++ MyDispo/src/Form/ModeleMailType.php
@@ -6,17 +6,24 @@ use App\Entity\ModeleMail;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class ModeleMailType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('sujet')
-            ->add('contenu')
-            ->add('nom')
-
-        ;
+            ->add('nom',TextType::class, array(
+      'label' => 'Nom du modèle de mail',
+      ))
+            ->add('sujet',TextType::class, array(
+      'label' => 'Sujet',
+      ))
+            ->add('contenu',TextareaType::class, array(
+      'label' => 'Message',
+      ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
